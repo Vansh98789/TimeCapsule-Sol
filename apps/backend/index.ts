3618cc59f0e8ee047afbcf5f5729f5ee824e3c3d
Vansh Maheshwari <121272010+Vansh98789@users.noreply.github.com>
2026-02-22 10:39:46 +0530

    added live link in backend

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -12,7 +12,7 @@ const upload = multer({ storage: multer.memoryStorage() })
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://time-capsule-sol-frontend-2j8z.vercel.app/",
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
